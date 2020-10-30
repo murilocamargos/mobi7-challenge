aaf1577cfda58aab12003dff0e3ae9aa2af2ca51
Murilo Camargos <murilo.camargosf@gmail.com>
2020-10-30 12:22:09 -0300

    Add function to compute center and zoom automatically

--- app.py
+++ app.py
@@ -4,6 +4,7 @@ from flask import render_template
 from flask import jsonify
 from flask import request
 import pandas as pd
+import numpy as np
 
 
 app = Flask(__name__)
@@ -29,6 +30,74 @@ def preprocess_positions_file():
     pos.loc[~pos.parado, 'tempo_andando'] = pos[~pos.parado].tempo
     
     return pos
+
+
+def zoom_center(lons: tuple=None, lats: tuple=None, lonlats: tuple=None,
+        format: str='lonlat', projection: str='mercator',
+        width_to_height: float=2.0) -> (float, dict):
+    """Finds optimal zoom and centering for a plotly mapbox.
+    Must be passed (lons & lats) or lonlats.
+    Temporary solution awaiting official implementation, see:
+    https://github.com/plotly/plotly.js/issues/3434
+    https://stackoverflow.com/questions/63787612/plotly-automatic-zooming-for-mapbox-maps
+    
+    Parameters
+    --------
+    lons: tuple, optional, longitude component of each location
+    lats: tuple, optional, latitude component of each location
+    lonlats: tuple, optional, gps locations
+    format: str, specifying the order of longitud and latitude dimensions,
+        expected values: 'lonlat' or 'latlon', only used if passed lonlats
+    projection: str, only accepting 'mercator' at the moment,
+        raises `NotImplementedError` if other is passed
+    width_to_height: float, expected ratio of final graph's with to height,
+        used to select the constrained axis.
+    
+    Returns
+    --------
+    zoom: float, from 1 to 20
+    center: dict, gps position with 'lon' and 'lat' keys
+
+    >>> print(zoom_center((-109.031387, -103.385460),
+    ...     (25.587101, 31.784620)))
+    (5.75, {'lon': -106.208423, 'lat': 28.685861})
+    """
+    if lons is None and lats is None:
+        if isinstance(lonlats, tuple):
+            lons, lats = zip(*lonlats)
+        else:
+            raise ValueError(
+                'Must pass lons & lats or lonlats'
+            )
+    
+    maxlon, minlon = max(lons), min(lons)
+    maxlat, minlat = max(lats), min(lats)
+    center = {
+        'lon': round((maxlon + minlon) / 2, 6),
+        'lat': round((maxlat + minlat) / 2, 6)
+    }
+    
+    # longitudinal range by zoom level (20 to 1)
+    # in degrees, if centered at equator
+    lon_zoom_range = np.array([
+        0.0007, 0.0014, 0.003, 0.006, 0.012, 0.024, 0.048, 0.096,
+        0.192, 0.3712, 0.768, 1.536, 3.072, 6.144, 11.8784, 23.7568,
+        47.5136, 98.304, 190.0544, 360.0
+    ])
+    
+    if projection == 'mercator':
+        margin = 3
+        height = (maxlat - minlat) * margin * width_to_height
+        width = (maxlon - minlon) * margin
+        lon_zoom = np.interp(width , lon_zoom_range, range(20, 0, -1))
+        lat_zoom = np.interp(height, lon_zoom_range, range(20, 0, -1))
+        zoom = round(min(lon_zoom, lat_zoom), 2)
+    else:
+        raise NotImplementedError(
+            f'{projection} projection is not implemented'
+        )
+    
+    return zoom, center
 
 
 pos = preprocess_positions_file()
@@ -57,12 +126,14 @@ def api_total_time():
 def api_get_path():
     placa = request.args.get('placa')
     df = pos.loc[pos.placa == placa]    
-    res = {'latlon': []}
+    res = {'latlon': [], 'zoom': 1, 'center': [0, 0]}
 
     if df.shape[0] > 0:
         df = df.sort_values(by='data_posicao')
         res['latlon'] = df[['latitude', 'longitude']].values.tolist()
     
+    res['zoom'], res['center'] = zoom_center(list(df.longitude.values), list(df.latitude.values))
+
     return jsonify(res)
 
 
