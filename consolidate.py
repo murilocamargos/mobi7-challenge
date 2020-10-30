8b8d0914db5fcc816a2466d649997295ae5fbbda
Murilo Camargos <murilo.camargosf@gmail.com>
2020-10-30 14:11:21 -0300

    Add function to find POIs containing a measurement

--- consolidate.py
+++ consolidate.py
@@ -1,4 +1,5 @@
 import pandas as pd
+from geopy.distance import geodesic
 
 
 def get_data():
@@ -19,6 +20,71 @@ def get_data():
     return pos, poi
 
 
+def feature_eng(pos):
+    """Add and transform features in the positions dataset to facilitate
+    the analysis.
+
+    Parameters
+    ----------
+    pos : pd.DataFrame
+        A data frame with the available position and state measurements
+        from all vehicles.
+    
+    Returns
+    -------
+    pos : pd.DataFrame
+        The same dataframe with the modified features.
+    """
+    # Add column to identify stopped vehicles
+    pos['parado'] = (pos.velocidade < 5) & (~pos.ignicao)
+
+    # Convert all date strings to datetime
+    pos.data_posicao = pos.data_posicao.apply(lambda x: pd.to_datetime(x[:-25]))
+
+    # Compute the ellapsed time between measurements for each vehicle
+    for placa in pos.placa.unique():
+        pos.loc[pos['placa'] == placa] = pos.loc[pos['placa'] == placa].\
+            sort_values(by='data_posicao')
+        pos.loc[pos['placa'] == placa, 'tempo'] = pos[pos['placa'] == placa].\
+            data_posicao.diff().apply(lambda x: x.seconds)
+
+    # Compute stopped and moving times
+    pos['tempo_parado'] = 0
+    pos['tempo_andando'] = 0
+    pos.loc[pos.parado, 'tempo_parado'] = pos[pos.parado].tempo
+    pos.loc[~pos.parado, 'tempo_andando'] = pos[~pos.parado].tempo
+
+    return pos
+
+
+def find_pois(measurement, list_of_pois):
+    """Find the POIs in the database that includes a given measurement.
+    The distance function relies on a ellipsoidal model of the earth to
+    find the shortest distance on the earth's surface.
+    https://geopy.readthedocs.io/en/stable/#module-geopy.distance
+    
+    Parameters
+    ----------
+    measurement : pd.DataFrame
+        An item of the pos data set containing latitude and longitude measures.
+
+    poi : pd.DataFrame
+        A data frame with all positions of interest with name, radius and
+        coordinates.
+
+    Returns
+    -------
+    pois : str
+        A comma separated list of all POIs that contains the measurement.
+    """
+    pois = list_of_pois.apply(lambda poi: geodesic(
+        (measurement.latitude, measurement.longitude),
+        (poi.latitude, poi.longitude)).meters <= poi.raio, axis=1)
+    pois = ','.join(poi[pois].nome)
+    return pois
+
+
 if __name__ == '__main__':
     pos, poi = get_data()
-    print(pos, poi)
+    pos = feature_eng(pos)
+    print(find_pois(pos.iloc[0], poi))
