546eca320007dfbe731c9580c2431a0fa29d8649
Murilo Camargos <murilo.camargosf@gmail.com>
2020-11-03 14:32:12 -0300

    Run command without setting up server

--- app.py
+++ app.py
@@ -128,9 +128,9 @@ def api_get_time():
 if __name__ == "__main__":
     if '--consolidate' in sys.argv:
         consolidate_results('./data')
-
-    app.run(
-        host=os.environ.get("BACKEND_HOST", "127.0.0.1"),
-        port=5000,
-        debug=True,
-    )
+    else:
+        app.run(
+            host=os.environ.get("BACKEND_HOST", "127.0.0.1"),
+            port=5000,
+            debug=True,
+        )
